7afc965752840faf2b793e858e41de756d2d0e75
Eder Hernández <dariotecsoft90@gmail.com>
2025-04-14 21:35:47 -0500

    Modifity code for double roll en activity

--- app/src/main/java/com/example/dicerollerparty/MainActivity.kt
+++ app/src/main/java/com/example/dicerollerparty/MainActivity.kt
@@ -6,9 +6,11 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
@@ -23,6 +25,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.dicerollerparty.ui.theme.DiceRollerPartyTheme
 
 class MainActivity : ComponentActivity() {
@@ -45,6 +48,8 @@ class MainActivity : ComponentActivity() {
 fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
 
     var result by remember { mutableIntStateOf(1) }
+    var result2 by remember { mutableIntStateOf(1) }
+
     val imageResource = when(result) {
         1 -> R.drawable.dice_1
         2 -> R.drawable.dice_2
@@ -54,19 +59,43 @@ fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
         else -> R.drawable.dice_6
     }
 
+    val imageResource2 = when(result2) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        else -> R.drawable.dice_6
+    }
+
     Column (
         modifier = modifier,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Image(
-            painter = painterResource(imageResource),
-            contentDescription = result.toString()
-        )
+        Row {
+            Image(
+                painter = painterResource(imageResource),
+                contentDescription = result.toString(),
+                modifier = Modifier
+                    .size(180.dp, 250.dp)
+            )
+            Image(
+                painter = painterResource(imageResource2),
+                contentDescription = result2.toString(),
+                modifier = Modifier
+                    .size(180.dp, 250.dp)
+            )
+        }
         Spacer(modifier = Modifier.height(16.dp))
         Button(
-            onClick = {result = (1..6).random()}
+            onClick = {
+                result = (1..6).random()
+                result2 = (1..6).random()
+            },
+            modifier = Modifier
+                .size(120.dp, 50.dp)
         ) {
-            Text(stringResource(R.string.roll))
+            Text(stringResource(R.string.roll), fontSize = 18.sp)
         }
     }
 
